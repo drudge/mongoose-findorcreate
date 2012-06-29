5f73e2a26e4c15f70929769e05aa0c70bde776a3
Matt Roman <romanmt@gmail.com>
2012-06-29 18:46:22 -0500

    added custom messages for validation errors

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-module.exports = exports = function superGoosePlugin(schema) {
+var _ = require('underscore') 
+_.str = require('underscore.string');
+_.mixin(_.str.exports());
+
+module.exports = exports = function superGoosePlugin(schema, options) {
+  var messages = options.messages
   schema.statics.findOrCreate = function findOrCreate(object, callback) {
     var self = this;
     this.findOne(object, function(err, result) {
@@ -11,5 +16,13 @@ module.exports = exports = function superGoosePlugin(schema) {
         });
       }
     })
+  }
+
+  schema.statics.errors = function errors(errors, callback) {
+    errors = _.toArray(errors.errors)
+    errors = _.map(errors, function(error) {
+       return _.sprintf(messages[error.type], error.path) 
+    })
+    callback(errors)
   }
 }
--- test/index_test.js
+++ test/index_test.js
@@ -6,15 +6,19 @@ var mocha = require('mocha')
 
 mongoose.connect('mongodb://localhost:27017/supergoose')
 mongoose.connection.on('error', function (err) {
-    console.error('MongoDB error: ' + err.message);
-      console.error('Make sure a mongoDB server is running and accessible by this application')
+  console.error('MongoDB error: ' + err.message);
+  console.error('Make sure a mongoDB server is running and accessible by this application')
 });
 
 var ClickSchema = new Schema({
-  ip : String
+  ip : {type: String, required: true}
 })
 
-ClickSchema.plugin(supergoose);
+var messages =  {
+  'required': "%s is required"
+}
+
+ClickSchema.plugin(supergoose, {messages: messages});
 var Click = mongoose.model('Click', ClickSchema);
 
 after(function(done) {
@@ -39,6 +43,17 @@ describe('findOrCreate', function() {
       click.ip.should.eql('127.0.0.1')
       Click.count({}, function(err, num) {
         num.should.equal(1)
+        done();
+      })
+    })
+  })
+})
+
+describe('errorMessages', function() {
+  it("creates an array of of custom errors", function(done) {
+    Click.create({}, function(err, val) {
+      Click.errors(err, function(messages) {
+        messages.should.eql(['ip is required'])
         done();
       })
     })
