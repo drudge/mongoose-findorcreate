04723fc694ce11475684fc132ec99748613e9212
Ryan Clarke <clarkie.ryan@gmail.com>
2015-12-30 18:22:00 +0000

    Adding in support for promises if no callback is passed. Adding in tests for promises support.

--- test/index_test.js
+++ test/index_test.js
@@ -24,43 +24,64 @@ ClickSchema.plugin(findOrCreate);
 var Click = mongoose.model('Click', ClickSchema);
 
 after(function(done) {
-  mongoose.connection.db.dropDatabase()
+  mongoose.connection.db.dropDatabase();
   done();
 })
 
 describe('findOrCreate', function() {
+
+  it("Should output a promise if no callbacks are passed", function(done){
+    var oneArgPromise = Click.findOrCreate({ip: '127.0.0.4'});
+    (oneArgPromise).should.be.a.Promise();
+
+    var twoArgPromise = Click.findOrCreate({ip: '127.1.1.4'}, {ip: '127.1.1.4'});
+    (twoArgPromise).should.be.a.Promise();
+
+    done();
+  });
+
+  it("The request should be fulfilled", function(){
+    var promise = Click.findOrCreate({ip: '127.0.0.3'});
+    return (promise).should.be.fulfilled();
+  });
+
+  // it("Returns the object from the promise", function(){
+
+  // });
+
   it("should create the object if it doesn't exist", function(done) {
     Click.findOrCreate({ip: '127.0.0.1'}, function(err, click) {
       click.ip.should.eql('127.0.0.1')
-      Click.count({}, function(err, num) {
-        num.should.equal(1)
+      Click.count({ip: '127.0.0.1'}, function(err, num) {
+        num.should.equal(1);
         done();
-      })
-    })
-  })
+      });
+    });
+  });
 
   it("returns the object if it already exists", function(done) {
     Click.create({ip: '127.0.0.2'}, function(){
       Click.findOrCreate({ip: '127.0.0.2'}, function(err, click) {
         click.ip.should.eql('127.0.0.2')
         Click.count({ip: '127.0.0.2'}, function(err, num) {
-          num.should.equal(1)
+          num.should.equal(1);
           done();
-        })
-      })
-    })
-  })
+        });
+      });
+    });
+  });
 
   it("should pass created as true if the object didn't exist", function(done) {
     Click.findOrCreate({ip: '127.1.1.1'}, function(err, click, created) {
       created.should.eql(true);
       done();
-    })
-  })
+    });
+  });
 
   it("should pass created as false if the object already exists", function(done) {
     Click.findOrCreate({ip: '127.1.1.1'}, function(err, click, created) {
       created.should.eql(false);
       done();
-    })
-  })})
+    });
+  });
+});
